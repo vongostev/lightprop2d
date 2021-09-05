ae71d1e99187cb2be5d964d88c1eeb89e875eb16
HauptMachine\vonGostev <gostev.pavel@physics.msu.ru>
2021-09-05 22:20:58 +0300

    Update generate_docs.py

--- docs/generate_docs.py
+++ docs/generate_docs.py
@@ -1,2 +1,9 @@
-from .markdoc import MarkDoc
-D2M = MarkDoc('../lightprop2d/beam2d.py', './Reference.md')
+# Generate documentation using https://github.com/vongostev/markdoc module
+import os
+import sys
+
+sys.path.append(os.path.abspath('../../markdoc'))
+
+from markdoc import MarkDoc
+
+MarkDoc('../lightprop2d/beam2d.py', './Reference.md')
